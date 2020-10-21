5a398dabaf788d28f8077f1525997183215f5a54
JoeLystein <70919712+JoeLystein@users.noreply.github.com>
2020-10-21 00:21:03 -0500

    new pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Welcome from './Welcome';
+import Nav from './Nav';
+import Contact from './Contact';
+import About from './About';
+import Loginpage from './Loginpage';
+import Signup from './Signup'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Joel Itipo</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>App</h1>
+        <Welcome/>
+        <Nav/>
+        
+
     </div>
   );
 }
