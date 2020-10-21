48cff1ef362800a7bcd97545def1fe2cca2e614d
JoeLystein <70919712+JoeLystein@users.noreply.github.com>
2020-10-21 01:05:31 -0500

    Update of nav

--- src/App.js
+++ src/App.js
@@ -6,15 +6,21 @@ import Contact from './Contact';
 import About from './About';
 import Loginpage from './Loginpage';
 import Signup from './Signup'
+import {BrowserRouter as Router,Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App"> 
-        <Nav/>
-        <Contact/>
-        <About/> 
-        <Loginpage/>
-        <Signup/>
+      <Router> 
+          <Nav/>
+            <Switch>
+              <Route path="/" exact component={Welcome}/>
+              <Route path="/Contact" component={Contact}/>
+              <Route path="/About" component={About}/>
+              <Route path="/Loginpage" component={Loginpage}/>
+              <Route path="/Signup" component={Signup}/>
+            </Switch>
+      </Router>
 
 
     </div>
--- src/Nav.js
+++ src/Nav.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import './App.css';
+import { Link } from 'react-router-dom'; 
 
 function Nav() {
+
+  const navStyle={
+      color: 'white'
+  };
   return (
     <nav>
         <h3>Logo</h3>
-        <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Login</li>
-            <li>Contact us</li>
+        <ul className="nav-links">
+            <Link style={navStyle} to='/'>
+                <li>Home</li>
+            </Link>
+            
+            <Link style={navStyle} to='/About'>
+                <li>About</li>
+            </Link>
+            
+            <Link style={navStyle} to='/Loginpage'>
+                <li>Login</li>
+            </Link>
+            
+            <Link style={navStyle} to='/Contact'>
+                <li>Contact us</li>
+            </Link>
+            
         </ul>
     </nav>
   );
