aeb26d91ef7998f8b7f8f29ae9bf64f6047aa19d
Joel <itipojoel@gmail.com>
2020-10-21 12:30:32 -0500

    registratio

---
+++ component/backgrd.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './CSS/backgrd.css';
+import back from './PSBMS.png'
+
+
+class backgrd extends React.Component {
+  render() {
+    return (
+      <div className="roB">
+        
+      </div>
+    );
+  } 
+}
+
+  export default backgrd;
