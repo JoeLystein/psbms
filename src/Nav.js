d90bdee38120ba2cdf82ebce543e56861b68cb75
JoeLystein <70919712+JoeLystein@users.noreply.github.com>
2020-10-21 00:21:24 -0500

    Updates

---
+++ src/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function Nav() {
+  return (
+    <nav>
+        <h3>Logo</h3>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Login</li>
+            <li>Contact us</li>
+        </ul>
+    </nav>
+  );
+}
+
+export default Nav;
